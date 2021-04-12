cd4eafd16db0326ccc5a3ce1351c3655d909d5a6
bcy <bcygsws@163.com>
2021-04-13 02:52:46 +0800

    .eslintrc文件配置

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router';
 import store from './store';
 // 引入 初始化样式-消除默认样式
 import '../public/css/reset.css';
+
+// new Vue({
+//   el: '#app',
+//   router,
+//   store,
+//   render: h => h(App)
+// });
+// 上述代码等价于
 new Vue({
   router,
   store,
